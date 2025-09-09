13c625502b8c1fec643f611ef04d984af18b8013
winstoncda <olivier.pro.cda@gmail.com>
2025-09-09 08:56:13 +0200

    récupére données de modèles liés

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -19,4 +19,14 @@ export const createBlog = async (req, res) => {
 };
 
 // Lister tous les blogs
-export const getAllBlogs = async (req, res) => {};
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+      .populate("author", "username email")
+      .sort({ createdAt: -1 });
+    console.log(blogs);
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -5,6 +5,7 @@ import { protect } from "../middlewares/authMidlleware.js";
 const router = express.Router();
 
 router.get("/", getAllBlogs);
+
 // on passe par le middleware avant d'aller sur le controller
 // si c'est valide, l'utilisateur est attaché à la requête
 router.post("/", protect, createBlog);
