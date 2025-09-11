8f677be1c8d83ee77aaf0581d1766cf2862269aa
winstoncda <olivier.pro.cda@gmail.com>
2025-09-11 13:42:12 +0200

    delete blog rating

--- src/controllers/rating.controller.js
+++ src/controllers/rating.controller.js
@@ -24,3 +24,18 @@ export const addOrUpdateRating = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+
+export const deleteRating = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { blogId } = req.params;
+    const rating = await Rating.findOne({ blog: blogId, author: _id });
+    if (!rating) {
+      return res.status(400).json({ message: "Note déjà supprimée" });
+    }
+    await Rating.findOneAndDelete({ blog: blogId, author: _id });
+    res.status(200).json({ message: "Note supprimée" });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/routes/rating.route.js
+++ src/routes/rating.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { addOrUpdateRating } from "../controllers/rating.controller.js";
+import {
+  addOrUpdateRating,
+  deleteRating,
+} from "../controllers/rating.controller.js";
 import { protect } from "../middlewares/authMidlleware.js";
 
 const router = express.Router();
 
 router.post("/:blogId", protect, addOrUpdateRating);
+router.delete("/:blogId", protect, deleteRating);
 
 export default router;
