b2f378543485976b4d223da5fe8eece3c8e6c317
winstoncda <olivier.pro.cda@gmail.com>
2025-09-18 14:06:37 +0200

    mise à jour profil et model

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    avatar: { type: String, required: null },
+    avatar: { type: String, default: null },
   },
   {
     timestamps: true,
